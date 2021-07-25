a27d979a9df383bb71cf83f76d0b1c99ebc594b2
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-25 13:17:43 +0600

    Abstract-class

---
+++ php-oop_Abstract_class/Abstract_class.php
@@ -0,0 +1,72 @@
+<?php 
+
+abstract class detail{  //abstract start
+    public $name;
+
+    abstract protected function num($a,$b);  //abstract method
+}
+
+class names extends detail{  //drived class abstact increment
+    public function num($a,$b){
+        echo $a+$b;
+    }
+}
+
+$calculate= new names();
+
+$calculate->num(50,40);
+
+//-----------------------------------------------//
+
+
+abstract class nishat{
+    public $name;
+
+    abstract protected function fri($num,$numb);
+    
+}
+
+class niloy extends nishat{
+    public function fri($num,$numb){
+        echo $num * $numb;
+    }
+}
+
+
+$calcu=new niloy();
+$calcu->fri(50,50);
+
+
+//------------------------------------------//
+
+abstract class fahim{  //abstract start
+    public $name;
+    abstract protected function num($a,$b);  //abstract method
+}
+
+class cyko extends fahim{  //abstract increment
+    public function num($a,$b){
+        echo $a + $b;
+    }
+}
+
+$cal= new cyko();
+$cal->num(50,90);
+
+//------------------------------------------------//
+
+abstract class bishal{
+    public $name;
+    abstract protected function number($a,$b);
+}
+
+
+class tamim extends bishal{
+    public function number($a,$b){
+        echo $a - $b;
+    }
+}
+
+$mix=new tamim();
+$mix->number(100,20);
+?>
