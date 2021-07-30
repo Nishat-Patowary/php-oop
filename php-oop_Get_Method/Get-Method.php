e9e2b946da62a0277f6542c141ceceb3e56d5168
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-30 10:24:37 +0600

    oop-Get_Method

---
+++ php-oop_Get_Method/Get-Method.php
@@ -0,0 +1,47 @@
+<?php 
+
+class nishat{
+  private $niloy= "This is a home";
+  public function __get($pro)
+  {
+      echo "You are tying not existing property<br>";
+  }
+}
+
+$mix= new nishat();
+$mix->niloy;
+
+//--------------------------------------------------------------------//
+
+class first{
+    private $one=["name"=>"Tanvirul","shift"=>"second","dep"=>"computer"];
+
+    public function __get($hyper)
+    {
+        echo "you are trying to aceess not existing property<br>";
+
+    }
+
+}
+
+$mixer= new first();
+print_r($mixer->one);
+
+//--------------------------------------------------------------------//
+
+class second{
+    private $info= ["Name"=>"Nishat","place"=>"dhaka","number"=>"Num"];
+
+    public function __get($detail)
+    {
+        if(array_key_exists($detail,$this->info)){
+            return $this->info[$detail];
+        }else{
+            return "The key($detail) is not define";
+        }
+    }
+}
+
+$mixed= new second();
+echo $mixed->place;
+?>
