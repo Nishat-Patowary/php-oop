bd9134515c21d6df1db35a191b4e05ff71feff5c
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-08-19 21:05:58 +0600

    Interface practice

--- php-oop_interface/Interface.php
+++ php-oop_interface/Interface.php
@@ -267,7 +267,6 @@ echo $child_teacher->first_child();
 echo $child_teacher->second_child();
 echo $child_teacher->third_child();
 
-//-----------------------------------------------------------------//
 
 
 ?>
