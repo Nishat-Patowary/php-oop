ccab89ff75148dbc5a12b5c36cb6c8090fe6650a
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-08-22 21:39:40 +0600

    trait_in_depth practice

---
+++ php-traits_in_depth/robot.php
@@ -0,0 +1,30 @@
+<?php 
+
+class robot{
+    private $maxrunspeed=0;
+    private $maxwalkingtime;
+
+    public function __construct($maxspeed,$maxwalkingtime)
+    {
+        $this->maxrunspeed = $maxspeed;
+        $this->maxwalkingtime =$maxwalkingtime;
+    }
+
+    public function getmaxspeed(){
+        return $this->maxrunspeed;
+    }
+
+    public function setmaxspeed($maxrunspeed): void{
+    $this->maxrunspeed = $maxrunspeed;
+    }
+
+    public function getmaxwalkingtime(){
+        return $this->maxwalkingtime;
+    }
+
+    public function setmaxwalkingtime($maxwalkingtime) : void{
+        $this->maxwalkingtime= $maxwalkingtime;
+    }
+}
+
+?>
